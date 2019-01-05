a1903d0bcc45e53799249d1c396ec190dff24ada
KeenWarrior <anujgargcse@gmail.com>
2019-01-05 16:02:35 +0530

    lectire- 9

--- src/com/codingblocks/lecture_9/Main.java
+++ src/com/codingblocks/lecture_9/Main.java
@@ -2,19 +2,25 @@ package com.codingblocks.lecture_9;
 
 public class Main {
     public static void main(String[] args) {
+        optimised(new StringBuilder(), "11111111111111111111111111111111111", 0);
 
+//        assign7q12("", "11111111111111111111111111111111111" );
     }
 
     public static void assign7q12(String processed, String unprocessed){
         if (unprocessed.isEmpty()){
-            System.out.println(processed);
+//            System.out.println(processed);
             return;
         }
 
+        int num = Integer.parseInt(unprocessed.substring(0, 1));
+        char ch = (char)('a' + num - 1);
+        assign7q12(processed + ch, unprocessed.substring(1));
+
         if (unprocessed.length() >= 2){
-            int num = Integer.parseInt(unprocessed.substring(0, 2));
+            num = Integer.parseInt(unprocessed.substring(0, 2));
             if (num <= 26){
-                char ch = (char)('a' + num - 1);
+                ch = (char)('a' + num - 1);
 
                 assign7q12(processed + ch, unprocessed.substring(2));
             }
@@ -24,8 +30,30 @@ public class Main {
             return;
         }
 
-        int num = Integer.parseInt(unprocessed.substring(0, 1));
-        char ch = (char)('a' + num - 1);
-        assign7q12(processed + ch, unprocessed.substring(1));
+
+    }
+
+    public static void optimised(StringBuilder processed, String unprocessed, int index){
+        if (index == unprocessed.length()){
+//            System.out.println(processed);
+            return;
+        }
+
+        char ch = (char)(unprocessed.charAt(index) - '0' - 1 + 'a');
+
+        processed.append(ch);
+        optimised(processed, unprocessed, index + 1);
+        processed.deleteCharAt(processed.length() - 1);
+
+
+        if (index < unprocessed.length() - 1){
+            ch = (char)((unprocessed.charAt(index) - '0') * 10 + (unprocessed.charAt(index + 1) - '0' - 1 + 'a'));
+
+            processed.append(ch);
+            optimised(processed, unprocessed, index + 2);
+            processed.deleteCharAt(processed.length() - 1);
+
+
+        }
     }
 }
