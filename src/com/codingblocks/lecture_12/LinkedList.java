6fce03c369755531379a41ef419eae021aa9a7e3
KeenWarrior <anujgargcse@gmail.com>
2019-01-19 09:50:52 +0530

    lectire- 12

--- src/com/codingblocks/lecture_12/LinkedList.java
+++ src/com/codingblocks/lecture_12/LinkedList.java
@@ -21,6 +21,117 @@ public class LinkedList {
 
     }
 
+    public String deleteFirst() throws Exception{
+        if (size == 0){
+            throw new Exception("Removing from empty list");
+        }
+
+        String value = head.value;
+
+        head = head.next;
+
+        if (head == null){
+            tail = head;
+        }
+
+        size--;
+
+        return value;
+    }
+
+    public String deleteLast() throws Exception{
+        if (size < 2){
+            return deleteFirst();
+        }
+
+        Node node = head;
+
+        while (node.next != tail){
+            node = node.next;
+        }
+
+        // now node have second last item
+
+        String value = tail.value;
+
+        // second last item is now last item
+        node.next = null;
+
+        // point tail to node
+        tail = node;
+
+        size--;
+
+        return value;
+    }
+
+    public void insertLast(String value){
+
+        if (size == 0){
+            insertFirst(value);
+            return;
+        }
+
+        // create new node
+        Node node = new Node(value);
+
+        // shift tail to new node
+        tail.next = node;
+        tail = node;
+
+        // increase size
+        size++;
+    }
+
+    public String get(int index) throws Exception{
+
+        if (index < 0 || index >= size){
+            throw new Exception("Index out of bound");
+        }
+
+        Node node = head;
+
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+
+        return node.value;
+    }
+
+    public Node getNode(int index) throws Exception{
+
+        if (index < 0 || index >= size){
+            throw new Exception("Index out of bound");
+        }
+
+        Node node = head;
+
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+
+        return node;
+    }
+
+    public void insert(int index, String value) throws Exception{
+        if (index == 0){
+            insertFirst(value);
+            return;
+        }
+
+        if (index == size){
+            insertLast(value);
+            return;
+        }
+
+        Node prev = getNode(index - 1);
+
+        Node node = new Node(value, prev.next);
+        prev.next = node;
+
+        size++;
+    }
+
     public void display(){
 
         Node node = head;
@@ -30,7 +141,27 @@ public class LinkedList {
             node = node.next;
         }
 
-        System.out.println("END\b");
+        System.out.println("END");
+    }
+
+    public String delete(int index) throws Exception{
+        if (index == 0){
+            return deleteFirst();
+        }
+
+        if (index == size - 1){
+            return deleteLast();
+        }
+
+        Node prev = getNode(index - 1);
+
+        String value = prev.next.value;
+
+        prev.next = prev.next.next;
+
+        size--;
+
+        return value;
     }
 
 
@@ -38,11 +169,11 @@ public class LinkedList {
         private String value;
         private Node next;
 
-        public Node(String value) {
+        private Node(String value) {
             this.value = value;
         }
 
-        public Node(String value, Node next) {
+        private Node(String value, Node next) {
             this.value = value;
             this.next = next;
         }
--- src/com/codingblocks/lecture_12/ListClient.java
+++ src/com/codingblocks/lecture_12/ListClient.java
@@ -2,11 +2,25 @@ package com.codingblocks.lecture_12;
 
 public class ListClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         LinkedList list = new LinkedList();
 
+
+        list.insertFirst("Mohini");
         list.insertFirst("Mohit");
+        list.insertLast("Harsh");
 
         list.display();
+
+//        list.deleteLast();
+
+        list.display();
+
+        list.insert(2, "Ritu");
+        list.insert(4, "Neha");
+
+        list.display();
+
+        System.out.println(list.get(0));
     }
 }
