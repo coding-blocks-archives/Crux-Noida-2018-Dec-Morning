d0714ea9bd8fca2dbbcf54e7ac2eef72733c0263
Keenarrior <anujgargcse@gmail.com>
2019-02-03 12:32:14 +0530

    l 15

--- src/com/codingblocks/lecture_16/Human.java
+++ src/com/codingblocks/lecture_16/Human.java
@@ -5,6 +5,22 @@ public class Human implements Comparable<Human> {
     private String name;
     private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
